6e6c22349d24cb65adc83da6f82c08795c62c88b
shamimalmamun-hub <shamimkhan620655@gmail.com>
2026-06-21 12:21:15 +0800

    feat: add briefcase icon to dependencies

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Property, Tutor } from '../types';
-import { MapPin, BookOpen, Clock, CalendarDays, Phone, LockKeyhole, MessageCircle } from 'lucide-react';
+import { MapPin, BookOpen, Clock, CalendarDays, Phone, LockKeyhole, MessageCircle, Briefcase } from 'lucide-react';
 import { useApp } from '../contexts/AppContext';
 import { useLanguage } from '../contexts/LanguageContext';
 
